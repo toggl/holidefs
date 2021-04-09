b35e98a46ba8071f2561945854ef1a95cc335946
kelvinst <kelvin.stinghen@gmail.com>
2021-04-09 15:37:42 -0300

    Generating one clause for every single definition loaded at compile time

--- lib/holidefs/definition/store.ex
+++ lib/holidefs/definition/store.ex
@@ -5,21 +5,22 @@ defmodule Holidefs.Definition.Store do
 
   alias Holidefs.Definition
 
-  @definitions for {c, n} <- Holidefs.locales(), do: Definition.load!(c, n)
-  @locale_keys Holidefs.locales() |> Map.keys() |> Enum.map(&Atom.to_string/1)
+  definitions = for {c, n} <- Holidefs.locales(), do: Definition.load!(c, n)
 
   @doc """
   Returns all the loaded definitions with their rules
   """
   @spec all_definitions :: [Holidefs.Definition.t()]
-  def all_definitions, do: @definitions
+  def all_definitions, do: unquote(Macro.escape(definitions))
 
   @doc """
   Returns the definitions for the given locale
   """
   @spec get_definition(Holidefs.locale_code()) :: Holidefs.Definition.t() | nil
-  def get_definition(l), do: Enum.find(@definitions, &(&1.code == get_locale_atom(l)))
+  for definition <- definitions do
+    def get_definition(unquote(definition.code)), do: unquote(Macro.escape(definition))
+    def get_definition(unquote(to_string(definition.code))), do: unquote(Macro.escape(definition))
+  end
 
-  defp get_locale_atom(l) when l in @locale_keys, do: String.to_existing_atom(l)
-  defp get_locale_atom(l) when is_binary(l) or is_atom(l), do: l
+  def get_definition(_), do: nil
 end
