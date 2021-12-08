95dd455a9ec96dcafeaffcdb6c7fdc13676b2a66
Adrien <adanselm@gmail.com>
2021-12-08 12:00:01 +0100

    Fixing observed day for Christmas in the US (#29)
    * Fixing observed day for Christmas in the US
    
    * Bump package version

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Holidefs.Mixfile do
   def project do
     [
       app: :holidefs,
-      version: "0.3.2",
+      version: "0.3.3",
       elixir: "~> 1.5",
       description: "Definition-based national holidays",
       source_url: @github_url,
