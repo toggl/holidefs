1fb912a00f98bf886e2b4130d2e8569eefcb751a
Michael Gao <michael.gao@toggl.com>
2024-04-15 17:51:29 +0800

    fix: CI

--- flake.nix
+++ flake.nix
@@ -1,31 +1,32 @@
 {
   description = "Holidefs: Definition-based national holidays in Elixir";
-
   inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs }:
-    let
-      elixirOverlay = final: prev: {
-        elixir-ls = (prev.elixir-ls.override {
-          elixir = prev.beam.packages.erlangR24.elixir_1_12;
-        });
-      };
-
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; overlays = [ elixirOverlay ]; };
-    in {
-      devShells.${system} = {
-        default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            nil
-            beam.packages.erlangR24.elixir_1_12
-            elixir_ls
-          ];
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        elixirOverlay = final: prev: {
+          elixir-ls = (prev.elixir-ls.override {
+            elixir = prev.beam.packages.erlangR24.elixir_1_12;
+          });
         };
 
-        ci = pkgs.mkShell {
-          buildInputs = with pkgs; [ beam.packages.erlangR24.elixir_1_12 ];
+        pkgs = import nixpkgs { inherit system; overlays = [ elixirOverlay ]; };
+      in {
+        devShells = {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              nil
+              beam.packages.erlangR24.elixir_1_12
+              elixir_ls
+            ];
+          };
+
+          ci = pkgs.mkShell {
+            buildInputs = with pkgs; [ beam.packages.erlangR24.elixir_1_12 ];
+          };
         };
-      };
-    };
+      }
+    );
 }
